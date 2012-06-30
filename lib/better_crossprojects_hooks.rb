166f5532f7484b99d6da6c31df08a004f0a4da81
jbbarth <jeanbaptiste.barth@gmail.com>
2012-06-30 19:54:15 +0200

    Removed abandoned code

--- lib/better_crossprojects_hooks.rb
+++ lib/better_crossprojects_hooks.rb
@@ -1,7 +1,4 @@
 class BetterUiHooks < Redmine::Hook::ViewListener
-  #adds our 'new issue' button logic
-  #render_on :view_layouts_base_content, :partial => 'better_crossprojects/view_layouts_base_content'
-
   #adds our css on each page
   def view_layouts_base_html_head(context)
     javascript_include_tag("better_crossprojects", :plugin => "redmine_better_crossprojects")
