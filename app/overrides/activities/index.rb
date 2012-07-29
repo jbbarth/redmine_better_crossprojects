af4c4bf82a7ba4545cd80bb5fa76963657e9789a
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-30 01:52:26 +0200

    Revert "Deface should only be used for html views, not API"
    This reverts commit a24c0322f82f8ee1ae5173bd1f1a6611a1a5fb6e.

--- app/overrides/activities/index.rb
+++ app/overrides/activities/index.rb
@@ -1,6 +1,6 @@
 #This uses the ruby syntax for deface
 #TODO: see if we can use the DSL and if it's better
-Deface::Override.new :virtual_path => 'activities/index.html',
+Deface::Override.new :virtual_path => 'activities/index',
                      :name         => 'add-crossproject-sidebar-to-activities-index',
                      :replace      => 'h3:contains("label_activity")',
                      :text         => '<%= @project ? content_tag(:h3, l(:label_activity)) : render("common/cross_sidebar") %>'
