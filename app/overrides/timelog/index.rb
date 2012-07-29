a24c0322f82f8ee1ae5173bd1f1a6611a1a5fb6e
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-30 01:19:25 +0200

    Deface should only be used for html views, not API

--- app/overrides/timelog/index.rb
+++ app/overrides/timelog/index.rb
@@ -1,6 +1,6 @@
 #This uses the ruby syntax for deface
 #TODO: see if we can use the DSL and if it's better
-Deface::Override.new :virtual_path => 'timelog/index',
+Deface::Override.new :virtual_path => 'timelog/index.html',
                      :name         => 'add-crossproject-sidebar-to-timelogs-index',
                      :insert_after => 'p.pagination',
                      :text         => '<% if @project.nil? && @issue.nil?; content_for :sidebar do %><%= render "common/cross_sidebar" %><% end; end %>'
