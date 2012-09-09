c63b2ed1173553ccfbe1fd3aaa292b46ff133b9f
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-09 11:24:05 +0200

    Added original partial fingerprints to deface overrides

--- app/overrides/activities/index.rb
+++ app/overrides/activities/index.rb
@@ -1,6 +1,7 @@
 #This uses the ruby syntax for deface
 #TODO: see if we can use the DSL and if it's better
 Deface::Override.new :virtual_path => 'activities/index',
+                     :original     => 'd3fb826a2bd63adc1df8f1fd9d6bd3706a548bab',
                      :name         => 'add-crossproject-sidebar-to-activities-index',
                      :replace      => 'h3:contains("label_activity")',
                      :text         => '<%= @project ? content_tag(:h3, l(:label_activity)) : render("common/cross_sidebar") %>'
--- app/overrides/timelog/index.rb
+++ app/overrides/timelog/index.rb
@@ -1,6 +1,7 @@
 #This uses the ruby syntax for deface
 #TODO: see if we can use the DSL and if it's better
 Deface::Override.new :virtual_path => 'timelog/index',
+                     :original     => 'ee65ebb813ba3bbf55bc8dc6279f431dbb405c48',
                      :name         => 'add-crossproject-sidebar-to-timelogs-index',
                      :insert_after => 'p.pagination',
                      :text         => '<% if @project.nil? && @issue.nil?; content_for :sidebar do %><%= render "common/cross_sidebar" %><% end; end %>'
