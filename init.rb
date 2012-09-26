4273cbc7760c5364ea48c38e284cf109c29dda2f
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-26 13:47:12 +0200

    Consistency with my other plugins

--- init.rb
+++ init.rb
@@ -12,8 +12,9 @@ Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides",
 
 Redmine::Plugin.register :redmine_better_crossprojects do
   name 'Redmine Better Crossprojects plugin'
-  author 'Jean-Baptiste BARTH'
   description 'This plugin will just provide better cross project views (based on 1.0.0(RC) ones)'
+  url 'https://github.com/jbbarth/redmine_better_crossprojects'
+  author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
   requires_redmine :version_or_higher => '2.0.3'
   version '0.1'
