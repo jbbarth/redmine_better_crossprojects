ef02954e9c3de4734f9f91808374171bc0cc8fc6
jbbarth <jeanbaptiste.barth@gmail.com>
2012-12-28 00:07:39 +0100

    Add activity informations in projects/index

--- assets/javascripts/better_crossprojects.js
+++ assets/javascripts/better_crossprojects.js
@@ -39,3 +39,9 @@ Effect.BlindLeft = function(element) {
   }, arguments[1] || { }));
 };
 */
+
+//draw activity sparklines on projects/index page
+$(function() {
+  $(".barchart").peity("bar", { colours: ["#aaa"], min: 0, max: 10,
+                                height:18, width: (52/2*(5+1)-1) });
+})
