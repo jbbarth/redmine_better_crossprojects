e51d0bd2cfad51277a39b22ff253c41fa0637558
jbbarth <jeanbaptiste.barth@gmail.com>
2012-12-28 00:49:40 +0100

    Add rspec and rspec-rails to the plugin

---
+++ spec/spec_helper.rb
@@ -0,0 +1,28 @@
+ENV["RAILS_ENV"] ||= 'test'
+
+#rails env
+require File.expand_path('../../../../config/environment', __FILE__)
+
+#test gems
+require 'rspec'
+require 'rspec/autorun'
+require 'rspec/mocks'
+require 'rspec/mocks/standalone'
+require 'pry'
+
+#load paths
+$:.<< File.expand_path('../../app/models', __FILE__)
+$:.<< File.expand_path('../../lib', __FILE__)
+
+#rspec base config
+RSpec.configure do |config|
+  config.mock_with :rspec
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
+end
+
+#fixtures!
+require 'rspec/rails'
+RSpec.configure do |config|
+  config.fixture_path = File.expand_path('../../../../test/fixtures', __FILE__)
+end
