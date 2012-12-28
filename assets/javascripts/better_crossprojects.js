3478189b79b7b29f37e82c29bad3bef7953fd107
jbbarth <jeanbaptiste.barth@gmail.com>
2012-12-28 02:06:51 +0100

    Add project description in projects/index, hidden by default

--- assets/javascripts/better_crossprojects.js
+++ assets/javascripts/better_crossprojects.js
@@ -2,4 +2,7 @@
 $(function() {
   $(".barchart").peity("bar", { colours: ["#aaa"], min: 0, max: 10,
                                 height:18, width: (52/2*(5+1)-1) });
+  $("table").on("click", ".more", function() {
+    $(this).closest("tr").next().toggle()
+  })
 })
