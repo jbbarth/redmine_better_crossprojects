fe37df8b9ddae0e2e040b89cc7c2f68b9dad1f3b
jbbarth <jeanbaptiste.barth@gmail.com>
2012-12-29 02:25:29 +0100

    Implement basic JS filter on project names in projects/index

--- assets/javascripts/better_crossprojects.js
+++ assets/javascripts/better_crossprojects.js
@@ -1,8 +1,30 @@
-//draw activity sparklines on projects/index page
 $(function() {
+  //draw activity sparklines on projects/index page
   $(".barchart").peity("bar", { colours: ["#aaa"], min: 0, max: 10,
                                 height:20, width: (27*(5+1)-1) });
+  //hide/show description of projects
   $("table").on("click", ".more", function() {
     $(this).closest("tr").next().toggle()
+  })
+  //focus on search field on load
+  $("#filter-by-name").focus()
+  //filter projects depending on input value
+  $("#filter-by-name").on("keyup", function() {
+    var needle = $.trim($(this).val().toLowerCase())
+    var count = 0
+    $(".projects-list .project-name a").each(function() {
+      var name = $(this).html().toLowerCase()
+      var $elem = $(this).closest('tr')
+      if (name.indexOf(needle) >= 0) {
+        $elem.show()
+        //restablish even/odd alternance
+        $elem.removeClass("even")
+        $elem.removeClass("odd")
+        $elem.addClass(["even", "odd"][count % 2])
+        count++
+      } else {
+        $elem.hide()
+      }
+    })
   })
 })
