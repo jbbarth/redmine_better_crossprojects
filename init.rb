8c654206990041a63982b4bd6822ae72889bdb86
jbbarth <jeanbaptiste.barth@gmail.com>
2013-04-01 21:00:18 +0200

    Fix plugin name in previous commit

--- init.rb
+++ init.rb
@@ -1,13 +1,5 @@
 require 'redmine'
 
-# Little hack for using the 'deface' gem in redmine:
-# - redmine plugins are not railties nor engines, so deface overrides in app/overrides/ are not detected automatically
-# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
-# - hack consists in adding "app/overrides" path of the plugin in Redmine's main #paths
-# TODO: see if it's complicated to turn a plugin into a Railtie or find something a bit cleaner
-Rails.application.paths["app/overrides"] ||= []
-Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
-
 Redmine::Plugin.register :redmine_better_crossprojects do
   name 'Redmine Better Crossprojects plugin'
   description 'This plugin will just provide better cross project views (based on 1.0.0(RC) ones)'
@@ -15,6 +7,7 @@ Redmine::Plugin.register :redmine_better_crossprojects do
   author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
   requires_redmine :version_or_higher => '2.0.3'
+  requires_redmine_plugin :redmine_base_deface, :version_or_higher => "0.0.1"
   version '0.1'
 end
 
