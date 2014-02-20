bcc071080f6efb0eb89e0a563e91dc66d0b94b91
Vincent Robert <vincent.robert@nanego.com>
2014-02-20 12:16:39 +0100

    Simplify filters addition: don't use an override when it's not necessary

--- app/overrides/projects/index.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-Deface::Override.new :virtual_path  => 'projects/index',
-                     :name          => 'add-search-form-to-projects-index',
-                     :replace  => '#projects_list_and_exports',
-                     :partial => "projects/search_form"
