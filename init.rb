f40383a0ceec8010238fa63dcf6e78a82cb00c69
Jean-Baptiste Barth <jeanbaptiste.barth@gmail.com>
2014-08-24 05:55:49 +0200

    Depend on redmine_base_deface and remove dirty hacks for deface integration

--- init.rb
+++ init.rb
@@ -6,14 +6,6 @@ ActionDispatch::Callbacks.to_prepare do
   require_dependency 'redmine_better_crossprojects/custom_field_patch'
 end
 
-# Little hack for using the 'deface' gem in redmine:
-# - redmine plugins are not railties nor engines, so deface overrides in app/overrides/ are not detected automatically
-# - deface doesn't support direct loading anymore ; it unloads everything at boot so that reload in dev works
-# - hack consists in adding "app/overrides" path of the plugin in Redmine's main #paths
-# TODO: see if it's complicated to turn a plugin into a Railtie or find something a bit cleaner
-Rails.application.paths["app/overrides"] ||= []
-Rails.application.paths["app/overrides"] << File.expand_path("../app/overrides", __FILE__)
-
 Redmine::Plugin.register :redmine_better_crossprojects do
   name 'Redmine Better Crossprojects plugin'
   description 'This plugin will just provide better cross project views (based on 1.0.0(RC) ones)'
@@ -22,11 +14,11 @@ Redmine::Plugin.register :redmine_better_crossprojects do
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
   requires_redmine :version_or_higher => '2.0.3'
   requires_redmine_plugin :redmine_base_select2, :version_or_higher => '0.0.1'
-  requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.1' if Rails.env.test?
+  requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
+  requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
   version '0.2'
   settings :default => { 'default_columns' => "name,role,users,issues,activity", 'show_description_as_a_column' => true },
            :partial => 'settings/redmine_plugin_better_crossprojects_settings'
-  requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.3' if Rails.env.test?
 end
 
 Redmine::MenuManager.map :project_menu do |menu|
