424967f0a714eb5f75d76bb93b8c38633f0e91f5
Vincent Robert <vincent.robert@nanego.com>
2015-06-23 09:17:02 +0200

    Update plugin version

--- init.rb
+++ init.rb
@@ -17,7 +17,7 @@ Redmine::Plugin.register :redmine_better_crossprojects do
   requires_redmine_plugin :redmine_base_select2, :version_or_higher => '0.0.1'
   requires_redmine_plugin :redmine_base_deface, :version_or_higher => '0.0.1'
   requires_redmine_plugin :redmine_base_rspec, :version_or_higher => '0.0.4' if Rails.env.test?
-  version '0.3'
+  version '0.4'
   settings :default => { 'default_columns' => "name,role,users,issues,activity", 'show_description_as_a_column' => true },
            :partial => 'settings/redmine_plugin_better_crossprojects_settings'
 end
