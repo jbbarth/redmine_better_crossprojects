bb438e4c6e6e06710cda7efefde1d38e0bee3da8
Vincent Robert <vincent.robert@nanego.com>
2016-02-22 14:38:04 +0100

    Improve compatibility with other plugins

--- lib/redmine_better_crossprojects/queries_helper_patch.rb
+++ lib/redmine_better_crossprojects/queries_helper_patch.rb
@@ -13,38 +13,41 @@ module QueriesHelper
     alias_method_chain :column_value, :better_crossprojects
   end
 
-  def csv_content(column, project)
-    case column.name
-      when :issues
-        value = ""
-      when :organizations
-        value = directions_map[project.id]
-      when :role
-        if @memberships[project.id].present?
-          value = @memberships[project.id].map(&:name).join(", ")
-        else
-          value = l(:label_role_non_member)
-        end
-      when :members
-        value = members_map[project.id]
-      when /role_(\d+)$/
-        if organizations_map[project.id.to_s] && organizations_map[project.id.to_s][$1]
-          value = organizations_map[project.id.to_s][$1].join(', ')
-        else
+  unless instance_methods.include?(:csv_content_with_better_crossprojects)
+    def csv_content_with_better_crossprojects(column, project)
+      case column.name
+        when :issues
           value = ""
-        end
-      when /function_(\d+)$/
-        if organizations_map[project.id.to_s] && organizations_map[project.id.to_s][column.name.to_s]
-          value = organizations_map[project.id.to_s][column.name.to_s].uniq.join(', ').html_safe
-        end
+        when :organizations
+          value = directions_map[project.id]
+        when :role
+          if @memberships[project.id].present?
+            value = @memberships[project.id].map(&:name).join(", ")
+          else
+            value = l(:label_role_non_member)
+          end
+        when :members
+          value = members_map[project.id]
+        when /role_(\d+)$/
+          if organizations_map[project.id.to_s] && organizations_map[project.id.to_s][$1]
+            value = organizations_map[project.id.to_s][$1].join(', ')
+          else
+            value = ""
+          end
+        when /function_(\d+)$/
+          if organizations_map[project.id.to_s] && organizations_map[project.id.to_s][column.name.to_s]
+            value = organizations_map[project.id.to_s][column.name.to_s].uniq.join(', ').html_safe
+          end
+        else
+          value = column.value(project)
+      end
+      if value.is_a?(Array)
+        value.collect {|v| csv_value(column, project, v)}.uniq.compact.join(', ')
       else
-        value = column.value(project)
+        csv_value(column, project, value)
+      end
     end
-    if value.is_a?(Array)
-      value.collect {|v| csv_value(column, project, v)}.uniq.compact.join(', ')
-    else
-      csv_value(column, project, value)
-    end
+    alias_method_chain :csv_content, :better_crossprojects
   end
 
   unless instance_methods.include?(:csv_value_with_better_crossprojects)
