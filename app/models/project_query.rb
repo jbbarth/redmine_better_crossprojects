37c28bb407929c7865bfdb597d2bd625d7c3a71b
Vincent Robert <vincent.robert@nanego.com>
2017-07-12 15:19:20 +0200

    Reproduce changes made in Redmine 3.4.0

--- app/models/project_query.rb
+++ app/models/project_query.rb
@@ -68,10 +68,21 @@ class ProjectQuery < Query
   # Returns a representation of the available filters for JSON serialization
   def available_filters_as_json
     json = {}
-    available_filters.each do |field, options|
+    available_filters.each do |field, filter|
+      options = {:type => filter[:type], :name => filter[:name]}
       options[:name] = l("field_project") if field == "id"
       options[:name] = l("label_member") if field == "member_id"
-      json[field] = options.slice(:type, :name, :values).stringify_keys
+      options[:remote] = true if filter.remote
+      if has_filter?(field) || !filter.remote
+        options[:values] = filter.values
+        if options[:values] && values_for(field)
+          missing = Array(values_for(field)).select(&:present?) - options[:values].map(&:last)
+          if missing.any? && respond_to?(method = "find_#{field}_filter_values")
+            options[:values] += send(method, missing)
+          end
+        end
+      end
+      json[field] = options.stringify_keys
     end
     json
   end
